123107809d0b0319d684a894f602a0a375fe5277
James L. Walsh <jloubierwalsh@gmail.com>
2020-01-22 20:46:39 -0500

    Deleted useless files

--- src/dawn/analysis/errors/SymbolAlreadyDefinedError.ts
+++
@@ -1,7 +0,0 @@
-import {ISymbol} from "@dawn/analysis/symbols/ISymbol";
-
-export class SymbolAlreadyDefinedError extends Error {
-  constructor(public readonly existingSymbol: ISymbol, public readonly newSymbol: ISymbol) {
-    super();
-  }
-}
